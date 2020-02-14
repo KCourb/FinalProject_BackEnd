2e448b31d8f5ecf11db36c1285b6b1363f0290bb
KCourb <courbet.kevin@gmail.com>
2020-02-14 12:48:51 +0100

    Creation projet

---
+++ src/test/java/com/projet/clinique/GestionCliniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.clinique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCliniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
