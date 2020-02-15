1e7a6c48668bc220943019be2ce408db09f34d8e
IN-ST-027 <steph.lalande@free.fr>
2020-02-15 17:36:16 +0100

    Associations

--- src/main/java/com/projet/clinique/entity/Departement.java
+++ src/main/java/com/projet/clinique/entity/Departement.java
@@ -2,9 +2,11 @@ package com.projet.clinique.entity;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,8 +17,8 @@ public class Departement {
 	private long idDepartement;
 	@Column
 	private String nomDepartement;
-	@Column
-	private List<Medecin> lstMedecin;  //OneToMany avec Medecin
+	@OneToMany(mappedBy = "departement", cascade=CascadeType.PERSIST)
+	private List<Medecin> lstMedecin;  
 	
 	public long getIdDepartement() {
 		return idDepartement;
