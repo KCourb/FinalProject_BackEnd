4e73e62da9f779fa242a9e548a6307a3250e566f
IN-ST-027 <steph.lalande@free.fr>
2020-02-15 17:01:09 +0100

    Fin des Getter/Setters + Constructeurs + toString

--- src/main/java/com/projet/clinique/entity/Consultation.java
+++ src/main/java/com/projet/clinique/entity/Consultation.java
@@ -21,5 +21,55 @@ public class Consultation {
 	private Facture facture;  //OneTone avec Facture
 	@Column
 	private List<Prescription> lstPrescription;   //OneToMany
+	public long getIdConsultation() {
+		return idConsultation;
+	}
+	public void setIdConsultation(long idConsultation) {
+		this.idConsultation = idConsultation;
+	}
+	public String getBilanPrescription() {
+		return bilanPrescription;
+	}
+	public void setBilanPrescription(String bilanPrescription) {
+		this.bilanPrescription = bilanPrescription;
+	}
+	public Rdv getRdv() {
+		return rdv;
+	}
+	public void setRdv(Rdv rdv) {
+		this.rdv = rdv;
+	}
+	public Facture getFacture() {
+		return facture;
+	}
+	public void setFacture(Facture facture) {
+		this.facture = facture;
+	}
+	public List<Prescription> getLstPrescription() {
+		return lstPrescription;
+	}
+	public void setLstPrescription(List<Prescription> lstPrescription) {
+		this.lstPrescription = lstPrescription;
+	}
+	public Consultation(long idConsultation, String bilanPrescription, Rdv rdv, Facture facture,
+			List<Prescription> lstPrescription) {
+		super();
+		this.idConsultation = idConsultation;
+		this.bilanPrescription = bilanPrescription;
+		this.rdv = rdv;
+		this.facture = facture;
+		this.lstPrescription = lstPrescription;
+	}
+	public Consultation() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Consultation [idConsultation=" + idConsultation + ", bilanPrescription=" + bilanPrescription + ", rdv="
+				+ rdv + ", facture=" + facture + ", lstPrescription=" + lstPrescription + "]";
+	}
+	
+	
+	
 
 }
--- src/main/java/com/projet/clinique/entity/Departement.java
+++ src/main/java/com/projet/clinique/entity/Departement.java
@@ -18,6 +18,33 @@ public class Departement {
 	@Column
 	private List<Medecin> lstMedecin;  //OneToMany avec Medecin
 	
+	public long getIdDepartement() {
+		return idDepartement;
+	}
+	public void setIdDepartement(long idDepartement) {
+		this.idDepartement = idDepartement;
+	}
+	public String getNomDepartement() {
+		return nomDepartement;
+	}
+	public void setNomDepartement(String nomDepartement) {
+		this.nomDepartement = nomDepartement;
+	}
+	public List<Medecin> getLstMedecin() {
+		return lstMedecin;
+	}
+	public void setLstMedecin(List<Medecin> lstMedecin) {
+		this.lstMedecin = lstMedecin;
+	}
+	public Departement() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Departement [idDepartement=" + idDepartement + ", nomDepartement=" + nomDepartement + ", lstMedecin="
+				+ lstMedecin + "]";
+	}
+	
 	
 
 }
