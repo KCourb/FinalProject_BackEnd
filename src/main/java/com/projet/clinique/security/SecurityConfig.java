6b492f2e728f22dec3005976fd3a6811f9f66706
KCourb <courbet.kevin@gmail.com>
2020-02-15 18:00:33 +0100

    SecurityConfig

---
+++ src/main/java/com/projet/clinique/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.projet.clinique.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private DataSource datasourceBean;
+		
+
+	public DataSource getDatasourceBean() {
+		return datasourceBean;
+	}
+
+	public void setDatasourceBean(DataSource datasourceBean) {
+		this.datasourceBean = datasourceBean;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(datasourceBean).
+		usersByUsernameQuery(" select username , password , activated from users where username=?").
+		authoritiesByUsernameQuery(
+				" select u.username , r.rolename from users u , roles r where u.idUser = r.idUser and u.username=?");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.httpBasic().and().authorizeRequests().
+		antMatchers("/Client").hasAuthority("ADMIN");
+		http.formLogin().loginPage("/login").passwordParameter("password").
+		usernameParameter("username").defaultSuccessUrl("/").failureUrl("/erreur");
+	}	
+
+}
