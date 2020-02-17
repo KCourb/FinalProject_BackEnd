1a3eddc5e84e34452624ca818cd3e56cec6b12fb
KCourb <courbet.kevin@gmail.com>
2020-02-17 10:32:20 +0100

    Jsp

---
+++ src/main/java/com/adaming/clinique/controller/loginController.java
@@ -0,0 +1,19 @@
+package com.adaming.clinique.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class loginController {
+	
+	@RequestMapping(value="/login")
+	public String getLogin() {
+		return"login";
+	}
+	
+	@RequestMapping(value="/erreur")
+	public String aux() {
+		return "erreur";
+	}
+
+}
