9fd90425e45d87c6b5e4216a305d1eb6c67753cc
KCourb <courbet.kevin@gmail.com>
2020-02-17 11:15:21 +0100

    jsp

---
+++ src/main/java/com/adaming/clinique/controller/homeController.java
@@ -0,0 +1,16 @@
+package com.adaming.clinique.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/")
+public class homeController {
+	
+	@RequestMapping(value = "/menu", method = RequestMethod.GET)
+	public String home() {
+		return "index";
+	}
+
+}
--- src/main/java/com/projet/clinique/entity/Prescription.java
+++ src/main/java/com/projet/clinique/entity/Prescription.java
@@ -19,11 +19,19 @@ public class Prescription {
 	private long idPrescription;
 	@Column
 	private String description;
+	@Column
+	private Boolean validee;
 	@ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
 	@JoinColumn(name = "consultation")
 	private Consultation consultation;   
 	
 	
+	public Boolean getValidee() {
+		return validee;
+	}
+	public void setValidee(Boolean validee) {
+		this.validee = validee;
+	}
 	public long getIdPrescription() {
 		return idPrescription;
 	}
@@ -51,11 +59,20 @@ public class Prescription {
 	public Prescription() {
 		super();
 	}
+
+	public Prescription(long idPrescription, String description, Boolean validee, Consultation consultation) {
+		super();
+		this.idPrescription = idPrescription;
+		this.description = description;
+		this.validee = validee;
+		this.consultation = consultation;
+	}
 	@Override
 	public String toString() {
-		return "Prescription [idPrescription=" + idPrescription + ", description=" + description + ", consultation="
-				+ consultation + "]";
+		return "Prescription [idPrescription=" + idPrescription + ", description=" + description + ", validee="
+				+ validee + ", consultation=" + consultation + "]";
 	}
+	
 	
 	
 
